5bde765f5174b68696dd1a8a460605d5c478db6c
Sachin Gupta <sgs.engineer@gmail.com>
2019-08-17 08:16:56 +0530

    On Iphone Animation

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.js-wp-2').waypoint(function(direction) {
+        $('.js-wp-2').addClass('animated slideInUp');
+    }, {
+        offset: '70%'
+    });
+
+    $('.iphone-btn').delay(2300).animate( {bottom: "+= -3"}, 300);
+});
